359a8fa86c27230c5a2b6354ebc7e4ec4690da1a
Sándor Negyeliczky <sandor.negyeliczky@gmail.com>
2019-08-07 09:06:36 +0200

    puzzle basics are ok

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
 console.log("hello gamer")
 
+let startButton = document.getElementById("startButton")
+startButton.addEventListener("click", createPuzzle)
+
+
+function createPuzzle() {
+    let gameTable = document.getElementById("mainTable");
+    for (let i = 1; i <=6; i++){
+        let puzzlePiece = document.createElement("div");
+        puzzlePiece.setAttribute("class","puzzleElement");
+        puzzlePiece.textContent= i;
+        gameTable.appendChild(puzzlePiece)
+
+    }
+
+}
+
 dragula([document.getElementById('mainTable')]);
